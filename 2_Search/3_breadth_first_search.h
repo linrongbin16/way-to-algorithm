1388681deee44ff7705854ec9a39c957caaed9bf
zhaochenyou <zhaochenyou@localhost.localdomain>
2015-04-19 23:31:02 +0800

    1

--- 2_Search/3_breadth_first_search.h
+++ 2_Search/3_breadth_first_search.h
@@ -13,56 +13,62 @@
 //像这样用队列来存储节点的邻节点的操作称为扩充该队列
 
 
-#include <string.h>
 #include <iostream>
-#include <deque.h>
+#include <deque>
+#include <string.h>
+#ifndef MAX
+#define MAX 500
+#endif
 
-void breadth_first_search(int m, int n, pair<int, int> beg)
+void breadth_first_search_print(std::pair<int, int> p)
+{
+	std::cout << "(row:" << p.first << ",column:";
+	std::cout << p.second << ")" << std::endl;
+}
+void breadth_first_search(int m, int n, std::pair<int, int> beg)
 {//矩阵有m行，n列，行下标从0到m-1，列下标从0到n-1
  //起点beg的行下标为beg.first(对应m行)，列下标为beg.seocnd(对应n列)
 	//visited标记节点是否被访问过
 	int visited[MAX][MAX];
 	memset(visited, 0, MAX * MAX *sizeof(int));
+	int beg_row = beg.first, beg_col = beg.second;
 
-	std::deque<pair<int, int> > q;
+	std::deque<std::pair<int, int> > q;
 	//将点进队之后就要将visit数组进行标识
 	//这样做可以“保证”队列中总是存储着：“将要被访问，而且从未访问过的点”
 	//何时对visited数组标记有很多种做法
 	//不同的方法会有不同的效果，但它们都存在缺陷，这些缺陷在bfs中并不会体现
 	//而在后面的双向广度搜索和启发式搜索中会体现出来，所以请记住这个细节
-	q.push(beg);
-	visited[beg.first][beg.second] = 1;
+	q.push_back(beg);
+	visited[beg_row][beg_col] = 1;
 	while(!q.empty()){
-		pair<int, int> p = q.front();
+		std::pair<int, int> p = q.front();
 		q.pop_front();
 		int p_row = p.first;
 		int p_col = p.second;
 
-		print_elem(p);
+		breadth_first_search_print(p);
 		//对p点四个方向的邻节点进行考虑
 		//还需判断矩阵越界
-		if(p.first - 1 >= 0 && !visited[p.first - 1][p.second]){
-			q.push(make_pair(p.first - 1, p.second));
-			visited[p.first - 1][p.second] = 1;
+		if(p_row - 1 >= 0 && !visited[p_row - 1][p_col]){
+			q.push_back(std::pair<int, int>(p_row - 1, p_col));
+			visited[p_row - 1][p_col] = 1;
 		}
-		if(p.first + 1 < m && !visited[p.first + 1][p.second]){
-			q.push(make_pair(p.first + 1, p.second));
-			visited[p.first + 1][p.second] = 1;
+		if(p_row + 1 < m && !visited[p_row + 1][p_col]){
+			q.push_back(std::pair<int, int>(p_row + 1, p_col));
+			visited[p_row + 1][p_col] = 1;
 		}
-		if(p.second - 1 >= 0 && !visited[p.first][p.second - 1]){
-			q.push(make_pair(p.first, p.second - 1));
-			visited[p.first][p.second - 1] = 1; 
+		if(p_col - 1 >= 0 && !visited[p_row][p_col - 1]){
+			q.push_back(std::pair<int, int>(p_row, p_col - 1));
+			visited[p_row][p_col - 1] = 1; 
 		}
-		if(p.second + 1 < n && !visited[p.first][p.second + 1]){
-			q.push(make_pair(p.first, p.second + 1));
-			visited[p.first][p.second + 1] = 1;
+		if(p_col + 1 < n && !visited[p_row][p_col + 1]){
+			q.push_back(std::pair<int, int>(p_row, p_col + 1));
+			visited[p_row][p_col + 1] = 1;
 		}
 	}
 }
-void breadth_first_search_print(pair<int, int> p)
-{
-	std::cout << "(row:" << p.first << ",column:";
-	std::cout << p.second << ")" << std::endl;
-}
+
+
 
 #endif
