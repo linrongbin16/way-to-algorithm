a8f5e56d386846c902b0363f74af41a7b2a39b90
zhaochenyou <zhaochenyou@localhost.localdomain>
2015-08-10 08:53:38 +0800

    1

--- 4_DynamicProgramming/4_TreeDynamic/1_binary_tree.h
+++ 4_DynamicProgramming/4_TreeDynamic/1_binary_tree.h
@@ -1,6 +1,5 @@
 #ifndef WAY_TO_ALGORITHM_DYNAMICPROGRAMMING_TREEDYNAMIC_BINARY_TREE_H
 #define WAY_TO_ALGORITHM_DYNAMICPROGRAMMING_TREEDYNAMIC_BINARY_TREE_H 1
-
 //二叉树动规
 //binary tree
 
@@ -31,10 +30,8 @@
 //本节引用了"不撞南墙不回头——树规总结" 作者"焦作一中信息学oy"
 
 
-
-
 //1)本文第一种方法是我自己的写法
-//该方法将动归常用的f数组也转化成节点的一个成员数组t_f
+//该方法将动归常用的f数组也转化成节点的一个成员数组m_f
 //请仔细体会这个数据结构 它更加完整的体现了树型动态规划中选择空间的分层本质
 //最初我本来准备用这样的数据结构完成这一节的所有代码
 //可是在后面的扩展问题中 该数据结构越来越成为函数接口的累赘 导致程序难以调试
@@ -56,7 +53,7 @@ struct binary_tree_node1
 };
 
 int binary_tree1(binary_tree_node1 *r, int resv)
-{//二叉树根节点为r 其中每个节点的权值数组t_f初始化为0 权值为t_value
+{//二叉树根节点为r 其中每个节点的权值数组m_f初始化为0 权值为m_value
  //二叉树保留resv个节点 返回二叉树最大权值
 	//递归终止条件
 	//处理叶子节点
@@ -65,25 +62,25 @@ int binary_tree1(binary_tree_node1 *r, int resv)
 	//若resv为0 则以该r节点为根节点的子树上没有孩子节点
 	if(resv == 0)
 		return(0);
-	//用于动规记录的f[][]数组存储在节点的t_f 所有节点的t_f初始化为0
-	//t_f[i]指代以当前节点r为根节点的子树上保留i个节点的最大权值
+	//用于动规记录的f[][]数组存储在节点的m_f 所有节点的m_f初始化为0
+	//m_f[i]指代以当前节点r为根节点的子树上保留i个节点的最大权值
 	//当前r保留resv节点的最大权值已经得到 直接返回
 	//这是动态规划的核心体现: 
 	//对之前已经得到的决策结果不再重复计算 而是存储起来直接查询
 	if(r->m_f[resv])
 		return(r->m_f[resv]);
 
-	//枚举左子树和右子树保留节点的个数 0 <= i < resv
+	//枚举左子树和右子树保留节点的个数 0<=i<resv
 	//左子树分配剩余的i个节点
-	//右子树必然剩余 resv-i-1 个节点(根节点自己 1 个)
+	//右子树必然剩余resv-i-1个节点(根节点自己1个)
 	for(int i = 0; i < resv; ++i){
-		int lmax = binary_tree1(r->m_leftchild, i);
-		int rmax = binary_tree1(r->m_rightchild, resv - i - 1);
-		//lmax指代左子树保留i个节点的最大权值
-		//rmax指代右子树保留resv-i-1个节点的最大权值
+		int left_max = binary_tree1(r->m_leftchild, i);
+		int right_max = binary_tree1(r->m_rightchild, resv - i - 1);
+		//left_max指代左子树保留i个节点的最大权值
+		//right_max指代右子树保留resv-i-1个节点的最大权值
 		//当r可以保留resv个节点 它给左子树和右子树分配节点的方法有resv-1种
 		//遍历这resv-1种方法 选取最大的之和即为r分配resv个节点的最大权值
-		r->m_f[resv] = max(r->m_f[resv], lmax + rmax);
+		r->m_f[resv] = max( r->m_f[resv], left_max + right_max );
 	}
 	//对于以r为根节点的树
 	//r树最大权值 = 左子树最大权值 + 右子树最大权值 + 根节点权值
@@ -116,9 +113,9 @@ int binary_tree2(binary_tree_node2 *r, int f[MAX][MAX], int resv)
 		return(f[r->m_index][resv]);
 
 	for (int i = 0; i < resv; ++i) {
-		int lmax = binary_tree2(r->m_leftchild, f, i);
-		int rmax = binary_tree2(r->m_rightchild, f, resv - i - 1);
-		f[r->m_index][resv] = max( f[r->m_index][resv], lmax + rmax );
+		int left_max = binary_tree2(r->m_leftchild, f, i);
+		int right_max = binary_tree2(r->m_rightchild, f, resv - i - 1);
+		f[r->m_index][resv] = max( f[r->m_index][resv], left_max + right_max );
 	}
 	f[r->m_index][resv] += r->m_value;
 	return( f[r->m_index][resv] );
