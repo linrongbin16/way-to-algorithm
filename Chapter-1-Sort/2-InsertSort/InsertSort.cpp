094acec93f8362d2fe98ce106dbd3d894f326779
zhaochenyou <zhaochenyou16@gmail.com>
2016-06-03 19:13:10 +0800

    update README.md

--- Chapter-1-Sort/2-InsertSort/InsertSort.cpp
+++ Chapter-1-Sort/2-InsertSort/InsertSort.cpp
@@ -1,67 +1,68 @@
-#include "3_insert_sort.h"
+#include "InsertSort.h"
 #include <iostream>
-
+#include <cassert>
 using namespace std;
 
 
-void printArray(int s[], int n, const char *msg)
+void AssertNotAscendingOrder(int s[MAX], int n)
 {
-    cout << msg << endl;
-    for (int i = 0; i < n; ++i)
-    {
-        cout << s[i] << ' ';
-        if (i % 10 == 9)
-            cout << endl;
+    bool res = true;
+    for (int i = 0; i < n-1; ++i) {
+        if (s[i] > s[i+1]) {
+            res = false;
+            break;
+        }
     }
-    cout << endl << endl;
+    assert(!res);
 }
-
-void printInsert(int s[], int n, const char *strname)
+void AssertAscendingOrder(int s[MAX], int n)
 {
-    cout << strname << ":" << endl;
-    cout << "before insert sort: " << endl;
-    printArray(s, n, strname);
-
-    insert_sort(s, 0, n);
-
-    cout << "after insert sort: " << endl;
-    printArray(s, n, strname);
-    cout << endl;
+    bool res = true;
+    for (int i = 0; i < n-1; ++i) {
+        if (s[i] > s[i+1]) {
+            res = false;
+            break;
+        }
+    }
+    assert(res);
+}
+void AssertInsertSort(int s[MAX], int n)
+{
+    AssertNotAscendingOrder(s, n);
+    InsertSort(s, 0, n);
+    AssertAscendingOrder(s, n);
 }
 
 int main()
 {
-    int s1[MAX] =
-    {
-        234,    10,     -41,    0,      34,     15,     64,     653,    -745,       74,
-        65,     26,     987,    -875,   -25,    -2861,  45,     16,     64,         46,
-        134,    10,     -41,    801,    32,     25,     63,     653,    -715,       18,
-        65,     36,     987,    -875,   -21,    -4861,  24,     16,     64,         46,
-        234,    10,     -41,    20,     38,     19,     40,     653,    -785,       23,
-        13,     33,     387,    -875,   -90,    -8871,  77,     66,     66,         34,
+    int s1[60] = {
+        234, 10, -41, 0, 34, 15, 64, 653, -745, 74,
+        65, 26, 987, -875, -25, -2861, 45, 16, 64, 46,
+        134, 10, -41, 801, 32, 25, 63, 653, -715, 18,
+        65, 36, 987, -875, -21, -4861, 24, 16, 64, 46,
+        234, 10, -41, 20, 38, 19, 40, 653, -785, 23,
+        13, 33, 387, -875, -90, -8871, 77, 66, 66, 34,
     };
 
-    int s2[20] =
-    {
-        234,    10,     -41,    20,     38,     19,     40,     653,    -785,       23,
-        65,     26,     987,    -875,   -25,    -2861,  45,     16,     64,         46,
+    int s2[20] = {
+        234, 10, -41, 20, 38, 19, 40, 653, -785, 23,
+        65, 26, 987, -875, -25, -2861, 45, 16, 64, 46,
     };
 
     int s3[2] = { 154, 46 };
 
-    int s4[50] =
-    {
-        234,    10,     -41,    0,      34,     15,     64,     653,    -745,       74,
-        65,     26,     987,    -875,   -25,    -2861,  45,     16,     64,         46,
-        134,    10,     -41,    801,    32,     25,     63,     653,    -715,       18,
-        65,     36,     987,    -875,   -21,    -4861,  24,     16,     64,         46,
-        234,    10,     -41,    20,     38,     19,     40,     653,    -785,       23,
+    int s4[50] = {
+        234, 10, -41, 0, 34, 15, 64, 653, -745, 74,
+        65, 26, 987, -875, -25, -2861, 45, 16, 64, 46,
+        134, 10, -41, 801, 32, 25, 63, 653, -715, 18,
+        65, 36, 987, -875, -21, -4861, 24, 16, 64, 46,
+        234, 10, -41, 20, 38, 19, 40, 653, -785, 23,
     };
 
-    printInsert(s1, MAX, "s1");
-    printInsert(s2, 20, "s2");
-    printInsert(s3, 2, "s3");
-    printInsert(s4, 50, "s4");
+    AssertInsertSort(s1, 0, 60);
+    AssertInsertSort(s2, 0, 20);
+    AssertInsertSort(s3, 0, 2);
+    AssertInsertSort(s4, 0, 50);
 
     return 0;
 }
--- Chapter-1-Sort/2-InsertSort/InsertSort.hpp
+++ Chapter-1-Sort/2-InsertSort/InsertSort.hpp
@@ -6,24 +6,34 @@
 #endif
 
 
+/* ----------------------------------------------------------------------*/
 /**
+ * @brief InsertSort 
  * 将无序序列s升序排列
- * @params s            无序序列
- * @params end          序列s的末尾下标加1，即左闭右开区间[beg, end)
- * @params end          序列s的末尾下标加1
+ *
+ * @param s[MAX]        无序序列
+ * @param beg           序列s的末尾下标加1，即左闭右开区间[beg, end)
+ * @param end           序列s的末尾下标加1
  */
+/* ----------------------------------------------------------------------*/
 void InsertSort(int s[MAX], int beg, int end)
 {
     for (int j = beg+1; j < end; ++j) {
-        int i = j-1;
-        int tmp = s[i + 1];
-        /* s[0]到s[j]找到i，满足s[j-1]<=s[i+1]<=s[j]注意j==-1的临界情况 */
-        while(j >= beg && s[j] > tmp)
-            --j;
-        //将s[i+1]插入s[j]位置
-        for(int k = i; k >= j + 1; --k)
-            s[k + 1] = s[k];
-        s[j + 1] = tmp;
+        int i;
+        int p = s[j];
+        /*s[0]到s[j]找到i，满足s[i-1]<=p<=s[i]*/
+        /*注意i=beg时不存在s[i-1]的情况 */
+        for (i = beg; i < j; ++i) {
+            if (i == beg && p <= s[i])
+                break;
+            else if (i > beg && s[i-1] <= p && p <= s[i])
+                break;
+        }
+        /*i即为p将被插入的位置*/
+        /*将i到j-1之间的元素向右移动一位*/
+        for(int k = j; k > i; --k)
+            s[k] = s[k-1];
+        s[i] = p;
     }
 }
 
