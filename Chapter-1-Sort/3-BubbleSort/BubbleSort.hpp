b85f76056d744b7776a5afe8918808ad4eef4b9b
zhaochenyou <zhaochenyou16@gmail.com>
2016-06-04 22:19:34 +0800

    add comment

--- Chapter-1-Sort/3-BubbleSort/BubbleSort.hpp
+++ Chapter-1-Sort/3-BubbleSort/BubbleSort.hpp
@@ -8,6 +8,16 @@
 using namespace std;
 
 
+/* ----------------------------------------------------------------------*/
+/**
+ * @brief BubbleSort 
+ * 冒泡排序
+ *
+ * @param s[MAX]        无序序列
+ * @param beg           序列s的末尾下标加1，即左闭右开区间[beg, end)
+ * @param end           序列s的末尾下标加1
+ */
+/* ----------------------------------------------------------------------*/
 void BubbleSort(int s[MAX], int beg, int end)
 {
     for (int i = end-1; i >= beg; --i)
