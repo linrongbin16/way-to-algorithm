c3d3857d9d1c6d29bfb6c16287a77f7d15643f84
zhaochenyou <zhaochenyou16@gmail.com>
2016-07-08 06:13:20 +0800

    update test for BruteForce

--- Chapter-2-Search/2-BruteForce/BruteForce.cpp
+++ Chapter-2-Search/2-BruteForce/BruteForce.cpp
@@ -1,6 +1,5 @@
 #include "BruteForce.hpp"
-#include <iostream>
-#include <algorithm>
+#include <cassert>
 using namespace std;
 
 #define TEST_N_MAX 4
@@ -9,8 +8,32 @@ using namespace std;
 int main()
 {
     int s[TEST_N_MAX];
-    for (int i = 0; i < TEST_M_MAX; i++) {
-        BruteForce(s, TEST_N_MAX, i);
+    for (int i = 1; i < TEST_M_MAX; i++) {
+        vector<int*> result = BruteForce(s, TEST_N_MAX, i);
+        assert(result.size() == pow(i, TEST_N_MAX));
+        int *s0, sum0;
+        s0 = result[0];
+        sum0 = 0;
+        for (int k = 0; k < TEST_N_MAX; k++) {
+            sum0 = sum0 * 10 + s0[k];
+        }
+        /* 第一个排列组合必然是[0, 0, 0, 0] */
+        assert(sum0 == 0);
+        for (int j = 0; j < result.size()-1; j++) {
+            int *s1, *s2;
+            int sum1 = 0, sum2 = 0;
+            s1 = result[j];
+            s2 = result[j + 1];
+            /* 排列组合的所有情况相当于依次递增的TEST_N_MAX位i进制正整数 */
+            for (int k = 0; k < TEST_N_MAX; k++) {
+                sum1 = sum1 * i + s1[k];
+                sum2 = sum2 * i + s2[k];
+            }
+            assert(sum1 == sum2 - 1);
+        }
+        for (int k = 0; k < result.size(); k++) {
+            delete[] result[k];
+        }
     }
 
     return 0;
--- Chapter-2-Search/2-BruteForce/BruteForce.hpp
+++ Chapter-2-Search/2-BruteForce/BruteForce.hpp
@@ -1,23 +1,16 @@
 #ifndef BRUTE_FORCE_HPP
 #define BRUTE_FORCE_HPP 1
 
-#include <iostream>
+#include <vector>
 using namespace std;
 #ifndef MAX
 #define MAX 60
 #endif
 
 
-void BruteForceOutput(int s[MAX], int beg, int end)
+vector<int*> BruteForce(int s[MAX], int n, int m)
 {
-    for (int i = beg; i < end; i++) {
-        cout << s[i] << " ";
-    }
-    cout << endl;
-}
-
-void BruteForce(int s[MAX], int n, int m)
-{
+    vector<int*> combinations;
     for (int i_0 = 0; i_0 < m; i_0++)
         for (int i_1 = 0; i_1 < m; i_1++)
             for (int i_2 = 0; i_2 < m; i_2++)
@@ -28,8 +21,12 @@ void BruteForce(int s[MAX], int n, int m)
                     s[2] = i_2;
                     /* ... */
                     s[n - 1] = i_n_1;
-                    BruteForceOutput(s, 0, n);
+                    int *r = new int[MAX];
+                    memcpy(r, s, MAX * sizeof(int));
+                    combinations.push_back(r);
                 }
+
+    return combinations;
 }
 
 
