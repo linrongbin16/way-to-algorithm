633c1c88d58202ba6b697eede71a2cbe2c78a7ad
zhaochenyou <zhaochenyou16@gmail.com>
2016-07-16 11:49:56 +0800

    add AStarSearch

--- Chapter-2-Search/6-AStarSearch/AStarSearch.cpp
+++ Chapter-2-Search/6-AStarSearch/AStarSearch.cpp
@@ -1,15 +1,128 @@
-#include <iostream>
-#include <algorithm>
-#include "5_a_heuristic_search.h"
+#include "AStarSearch.hpp"
+#include <cassert>
 using namespace std;
+
+bool IsAdjacentNode(PuNode a, PuNode b)
+{
+    int diff = 0;
+    int pos1, pos2;
+    for (int i = 0; i < 9; i++)
+        if (a.value[i] != b.value[i]) {
+            diff += 1;
+            if (diff == 1)
+                pos1 = i;
+            else
+                pos2 = i;
+        }
+    if (diff != 2)
+        return false;
+
+    if (a.value[pos1] != 'x' && a.value[pos2] != 'x')
+        return false;
+    if (b.value[pos1] != 'x' && b.value[pos2] != 'x')
+        return false;
+
+    return a.value[pos1] == b.value[pos2] && a.value[pos2] == b.value[pos1];
+}
+
+void UnitTest(PuNode beg, PuNode end)
+{
+    vector<PuNode> path = AStarSearch(beg, end);
+    assert(path.size() > 0);
+    assert(path[0] == beg);
+    assert(path[path.size()-1] == end);
+    for (int i = 0; i < path.size() - 1; i++) {
+        assert(IsAdjacentNode(path[i], path[i+1]));
+    }
+}
 
 int main()
 {
-	hs_node beg, end;
-	beg.m_status = "4132x6758";
-	end.m_status = "12345678x";
-	cout << endl << "A* heuristic search:" << endl;
-	a_heuristic_search(beg, end);	//5
+    PuNode beg, end;
 
-	return 0;
+    /* case 1:
+    beg      end
+    1 3 4    1 2 3
+    8 x 5    8 x 4
+    7 2 6    7 6 5
+    */
+    memset(&beg, 0, sizeof(beg));
+    memset(&end, 0, sizeof(end));
+    memcpy(beg.value, "1348x5726", 9);
+    memcpy(end.value, "1238x4765", 9);
+    UnitTest(beg, end);
+
+    /* case 2:
+    beg      end
+    2 3 1    1 2 3
+    7 x 8    8 x 4
+    6 5 4    7 6 5
+    */
+    memset(&beg, 0, sizeof(beg));
+    memset(&end, 0, sizeof(end));
+    memcpy(beg.value, "2317x8654", 9);
+    memcpy(end.value, "1238x4765", 9);
+    UnitTest(beg, end);
+
+    /* case 3:
+    beg      end
+    2 3 1    1 2 3
+    8 x 4    8 x 4
+    7 6 5    7 6 5
+    */
+    memset(&beg, 0, sizeof(beg));
+    memset(&end, 0, sizeof(end));
+    memcpy(beg.value, "2318x4765", 9);
+    memcpy(end.value, "1238x4765", 9);
+    UnitTest(beg, end);
+
+    /* case 4:
+    beg      end
+    1 2 3    2 3 1
+    8 x 4    8 x 4
+    7 6 5    7 6 5
+    */
+    memset(&beg, 0, sizeof(beg));
+    memset(&end, 0, sizeof(end));
+    memcpy(beg.value, "1238x4765", 9);
+    memcpy(end.value, "2318x4765", 9);
+    UnitTest(beg, end);
+
+    /* case 5:
+    beg      end
+    1 2 3    2 3 1
+    8 x 4    8 x 4
+    7 6 5    7 6 5
+    */
+    memset(&beg, 0, sizeof(beg));
+    memset(&end, 0, sizeof(end));
+    memcpy(beg.value, "1238x4765", 9);
+    memcpy(end.value, "2318x4765", 9);
+    UnitTest(beg, end);
+
+    /* case 6:
+    beg      end
+    2 8 3    1 2 3
+    1 x 4    8 x 4
+    7 6 5    7 6 5
+    */
+    memset(&beg, 0, sizeof(beg));
+    memset(&end, 0, sizeof(end));
+    memcpy(beg.value, "2831x4765", 9);
+    memcpy(end.value, "1238x4765", 9);
+    UnitTest(beg, end);
+
+    /* case 7:
+    beg      end
+    1 3 4    1 2 3
+    8 x 5    8 x 4
+    7 2 6    7 6 5
+    */
+    memset(&beg, 0, sizeof(beg));
+    memset(&end, 0, sizeof(end));
+    memcpy(beg.value, "1348x5726", 9);
+    memcpy(end.value, "1238x4765", 9);
+    UnitTest(beg, end);
+
+    return 0;
 }
--- Chapter-2-Search/6-AStarSearch/AStarSearch.hpp
+++ Chapter-2-Search/6-AStarSearch/AStarSearch.hpp
@@ -1,272 +1,176 @@
-#ifndef SEARCH_A_HEURISTIC_SEARCH_H
-#define SEARCH_A_HEURISTIC_SEARCH_H 1
-//A*启发式搜索算法
-//a heuristic search
+#ifndef A_STAR_SEARCH_HPP
+#define A_STAR_SEARCH_HPP 1
 
-//八数码问题
-//现有3*3矩阵  2  8  1
-//             3  7  x
-//             6  4  5
-//目标是将其变换为目标状态  1  2  3
-//                          4  5  6
-//                          7  8  x
-//每一次只能将x和其上下左右的相邻元素交换一次位置而其他数字不能随意改变位置
-//使用A*启发式搜索算法求出最少交换次数的变化经过(最短路径)
+/* 八数码问题 */
 
-//1)基础概念
-//启发式搜索
-//传统的dfs与bfs都是无差别的搜索所有节点的子空间
-//启发式搜索类似bfs 对某节点周围的相邻节点进行进一步搜索
-//但不同之处在于启发式搜索对这些待搜索的相邻节点进行一次评估
-//计算出这些待搜索的相邻节点到目标节点的距离
-//并按照距离将这些节点插入待搜索的节点队列中
-//而每次只取出队头节点即距离目标节点最近的节点进行搜索
-//启发式搜索将距离目标节点最近的节点设置为高优先级 从而降低时间复杂度
-//
-//A*搜索
-//A*搜索是对状态空间的搜索
-//所谓状态空间并不像之前的矩阵或序列这种几何空间而是时间上的状态
-//比如魔方从乱序变化为有序的状态 魔方的每个步骤都是一个状态
-//但是在八数码问题中有些状态可以转化为终点状态
-//而有些状态可能是死循环而永远无法转化为终点状态
-//
-//评估函数f
-//节点i的评估函数是f(i)=g(i)+h(i)
-//f(i)是节点i的评估距离
-//g(i)是从起点到节点i的路径的长度
-//h(i)是从节点i到终点的路径的长度的估计值
-//
-//2)具体实现
-//矩阵状态用string表示 每一个状态都可以看做空间中的一个节点
-//比如"12345678x"为终点目标状态 "123"是矩阵第1行 "456"第2行 "78x"第3行
-//"3124x7568"为起点初始状态 找出从起点到终点的最短变化次序
-//本问题中并非所有初始状态都能经过变化转化为终点状态
-//若不存在这样的变化路径则返回不存在 若存在则找出这样的最短变化路径
-//
-//评价函数f
-//对于一个矩阵状态i的评价值f(i)=g(i)+h(i)
-//g(i)
-//指代起始状态到状态i的路径长度 即经过变换的次数
-//可以对每次状态的变换进行累加计数
-//h(i)
-//指代从状态i到终点状态的估计路径长度
-//在这里设置为当前状态i和终点状态的不同字符的个数之和 即两状态的差异
-//比如"12345678x"和"1234567x8"的差异为2
-//每次优先选取f值最小的状态进行下一次搜索
-//状态也称为节点
-//
-//状态空间作为我们搜索的节点 也存在"已被访问"的问题
-//之前几节的算法中都会使用visited数组来标记一个节点是否已被访问来避免重复访问
-//当时间状态是节点时我们使用两个队列来记录未访问的节点和已访问的节点
-//称这两个队列为open和close队列(open队列记录未访问节点close队列记录以访问节点)
-//注意open队列是以评价值f作为队列优先级的
-//开始时将起始状态插入open队列队尾 close队列初始化为空 之后重复以下步骤
-//从open队头取一节点(该节点是open队列中距离目标状态最近的节点)
-//若该节点就是目标状态节点则直接返回结果
-//否则计算出该节点状态的所有相邻状态节点的评价值f
-//并只将close中没有的(未被访问)节点新加入open队列中 close中已存在的节点不再加入
-//直到open队列也没有找到目标状态则起始状态无解
-//这就是"A*搜索的状态空间搜索框架"
-
-//对于八数码或魔方这类状态空间的搜索 有一个通用的搜索框架
-//维护两个队列open和close
-//其中open保存当前待扩展的节点 close保存已经访问过的节点
-//其中open是以评价函数f为优先级的队列 初始时open入队起点
-//重复以下步骤直到open表为空
-//取出open中的头节点i 将节点i加入close中
-//若节点i是终点 沿着节点i的父节点指针回溯到起点即可得到最短变化路径
-//若节点i不是终点 扩展它的所有邻节点(即x与上下左右四个方向进行交换)加入open中
-//设它的一个邻节点为j 需要设置j的父节点指针是i从而可以从节点j回溯找回节点i
-//下一次循环时会将邻节点j加入close中
-//但仅仅依靠以上的操作仍然不够 状态空间中也存在"已被访问的节点"的问题
-//比如魔方经过不同步骤的变化却会得到相同的状态 是因为有些变化路径饶了"弯路"
-//假如close中已存在邻节点j说明节点j已被访问
-//这时需要比较这个将要加入队列的邻节点j和队列中已存在的节点j的评价函数f值
-//若将要加入的j比已存在的j的f值小 则将已存在的j替换为这个将要加入的j节点
-//这个操作是为了选择最短的变化路径 而不选择那些冗余的路径
-//重复上述步骤直到最终从open中取出的头节点是终点
-//沿着父节点指针即可回溯找到最短路径
-//若open为空时仍不能找到最短路径 则不存在这样的路径
-//若最终找到目标状态 则根据节点中的父节点指针可以在cloes队列中回溯找到整条路径
-
-//关于A*搜索更多内容请参考相关教材
-//本文引用了"启发式搜索和A*" 作者"phylips@bmy"
-
-
+#include <algorithm>
+#include <deque>
+#include <vector>
+#include <cstdlib>
+#include <climits>
+using namespace std;
 #ifndef MAX
 #define MAX 60
 #endif
 #ifndef INF
 #define INF INT_MAX
 #endif
-#include <algorithm>
-#include <deque>
-#include <cstring>
-#include <cstdlib>
-#include <iostream>
-#include <climits>
-using namespace std;
 
 
-struct hs_node
+struct PuNode
 {
-    string m_status;	//9 chars
-    int m_h;
-    int m_g;
-    hs_node *m_father_node;
+    char value[9];
+    int hScore;
+    int gScore;
+    char fatherValue[9];
 
-    bool operator==(const hs_node& node)
+    bool operator==(const PuNode &node_) const
     {
-        return(m_status == node.m_status && m_h == node.m_h
-               && m_g == node.m_g && m_father_node == node.m_father_node);
+        return memcmp(value, node_.value, 9) == 0;
+    }
+    bool operator!=(const PuNode &node_) const
+    {
+        return memcmp(value, node_.value, 9) != 0;
     }
 };
 
-bool heuristic_compare(hs_node a, hs_node b)
+/*
+ 将矩阵
+ 0 1 2
+ 3 4 5
+ 6 7 8
+ 表示为char[9]数组
+*/
+int direction[4] = { -3, 3, -1, 1 };
+
+int XPosition(PuNode a)
 {
-    return(a.m_g + a.m_h < b.m_g + b.m_h);
-}
-int x_position(hs_node a, char x)
-{
-    int index;
-    for(index = 0; index < 9; ++index)
-        if(a.m_status[index] == x)
+    int i;
+    for (i = 0; i < 9; i++)
+        if (a.value[i] == 'x')
             break;
-    return(index);
+    return i;
 }
-void left_swap(hs_node& a, int pos)
+void SwapPosition(PuNode &a, int pos1, int pos2)
 {
-    swap(a.m_status[pos - 1], a.m_status[pos]);
+    swap(a.value[pos1], a.value[pos2]);
 }
-void right_swap(hs_node& a, int pos)
+bool InsideBoundary(int pos)
 {
-    swap(a.m_status[pos + 1], a.m_status[pos]);
+    return pos >= 0 && pos < 9;
 }
-void up_swap(hs_node& a, int pos)
-{
-    swap(a.m_status[pos - 3], a.m_status[pos]);
-}
-void down_swap(hs_node& a, int pos)
-{
-    swap(a.m_status[pos + 3], a.m_status[pos]);
-}
-bool left_bound(int pos)
-{
-    return(pos == 0 || pos == 3 || pos == 6);
-}
-bool right_bound(int pos)
-{
-    return(pos == 2 || pos == 5 || pos == 8);
-}
-bool up_bound(int pos)
-{
-    return(pos == 0 || pos == 1 || pos == 2);
-}
-bool down_bound(int pos)
-{
-    return(pos == 6 || pos == 7 || pos == 8);
-}
-int heuristic_difference(hs_node a, hs_node b)
+int HScoreDiff(PuNode a, PuNode b)
 {
     int diff = 0;
-    for(int i = 0; i < 9; ++i)
-        if(a.m_status[i] != b.m_status[i])
+    for (int i = 0; i < 9; ++i)
+        if (a.value[i] != b.value[i])
             ++diff;
     return diff;
 }
-deque<hs_node>::iterator heuristic_minimum(deque<hs_node>& q, hs_node b)
+PuNode OpenQueueMinimum(deque<PuNode> &openQue, PuNode a)
 {
-    deque<hs_node>::iterator res;
+    PuNode res;
     int minf = INF;
-    for (deque<hs_node>::iterator i = q.begin(); i != q.end(); ++i)
-    {
-        int h = 0;
-        //从q队列中选取差异最小的状态进行下一次搜索
-        for (int j = 0; j < 9; ++j)
-            if (i->m_status[j] != b.m_status[j])
-                ++h;
-        if (minf > h + i->m_g)
-        {
-            minf = h + i->m_g;
-            res = i;
+    for (int i = 0; i < openQue.size(); i++) {
+        int h = HScoreDiff(openQue[i], a);
+        if (minf > h + openQue[i].gScore) {
+            minf = h + openQue[i].gScore;
+            res = openQue[i];
         }
     }
     return res;
 }
-void print_road(hs_node *pre)
+void OpenQueueErase(deque<PuNode> &openQue, PuNode a)
 {
-    if(pre->m_father_node != NULL)
-        print_road(pre->m_father_node);
-    cout << "(status:" << pre->m_status << ",length:" << pre->m_g << ")" << endl;
+    for (deque<PuNode>::iterator i = openQue.begin(); i != openQue.end(); i++) {
+        if (a == *i) {
+            openQue.erase(i);
+            break;
+        }
+    }
+}
+bool QueueContain(deque<PuNode> &que, PuNode a, deque<PuNode>::iterator &out_iter)
+{
+    for (deque<PuNode>::iterator i = que.begin(); i != que.end(); i++) {
+        if (a == *i) {
+            out_iter = i;
+            return true;
+        }
+    }
+    return false;
 }
 
-void a_heuristic_search(hs_node beg, hs_node end)
+void AStarPath(const deque<PuNode> &closeQue, PuNode beg, PuNode end, vector<PuNode> &path)
 {
-    //beg是起始矩阵状态 end是目标矩阵状态
-    //返回是否找到最短变化路径
-    //若找到则close队列的队尾节点为目标状态 沿节点的父指针回溯找到整条路径
-    deque<hs_node> open;
-    deque<hs_node> close;
-    close.clear();
-    //搜索的总体方式采用bfs
-    open.push_back(beg);
-    while (!open.empty())
-    {
-        //从open队列中选取距离end节点评价值f最小的节点minp进行搜索
-        //这个操作维护open队列的优先级
-        deque<hs_node>::iterator minp = heuristic_minimum(open, end);
-        //若该节点为目标节点则加入close队尾并直接返回
-        if (minp->m_status == end.m_status)
-        {
-            close.push_back(*minp);
-            print_road(&close.back());
-            return;
-        }
-        //否则继续搜索 close尾部加入minp open头部删除minp
-        close.push_back(*minp);
-        open.erase(minp);
-
-        //open继续扩展minp节点的邻节点
-        int pos = x_position(close.back(), 'x');
-        //next是minp节点上下左右四个方向的邻节点 先初始化为minp节点
-        hs_node next[4];
-        for(int i = 0; i < 4; ++i)
-            next[i] = close.back();
-
-        //这里的函数指针纯粹是为了缩短代码
-        //func_bound检测x的交换方向是否越界
-        bool (*func_bound[4])(int);
-        func_bound[0] = left_bound;
-        func_bound[1] = right_bound;
-        func_bound[2] = up_bound;
-        func_bound[3] = down_bound;
-        //func_swap进行x的交换操作
-        void (*func_swap[4])(hs_node&, int);
-        func_swap[0] = left_swap;
-        func_swap[1] = right_swap;
-        func_swap[2] = up_swap;
-        func_swap[3] = down_swap;
-        for (int i = 0; i < 4; ++i)
-            //判断x交换的方向是否越界
-            if (!func_bound[i](pos))
-            {
-                func_swap[i](next[i], pos);
-                //next[i]的父指针指向close的尾部元素
-                //即刚从open中出队 刚进入close尾部的那个节点
-                next[i].m_father_node = &close.back();
-                //增加该节点的路径长度
-                ++next[i].m_g;
-                //计算该节点到目标节点的距离
-                next[i].m_h = heuristic_difference(next[i], end);
-                //open_it为open迭代器 close_it为close迭代器
-                deque<hs_node>::iterator open_it;
-                deque<hs_node>::iterator close_it;
-                //若close中不存在next[i]这个节点则将新节点插入open队尾
-                if ( (close_it = find(close.begin(), close.end(), next[i])) == close.end() )
-                    open.push_back(next[i]);
+    deque<PuNode> duePath;
+    while (end != beg) {
+        duePath.push_front(end);
+        for (int i = 0; i < closeQue.size(); i++) {
+            if (memcmp(closeQue[i].value, end.fatherValue, 9) == 0) {
+                end = closeQue[i];
+                break;
             }
+        }
     }
-    //经过这么多状态还没找到那就是没得解了
+    duePath.push_front(beg);
+
+    for (int i = 0; i < duePath.size(); i++) {
+        path.push_back(duePath[i]);
+    }
+}
+
+vector<PuNode> AStarSearch(PuNode beg, PuNode end)
+{
+    deque<PuNode> openQue;
+    deque<PuNode> closeQue;
+    openQue.push_back(beg);
+    while (!openQue.empty()) {
+        PuNode min_node = OpenQueueMinimum(openQue, end);
+
+        if (min_node == end) {
+            closeQue.push_back(min_node);
+            vector<PuNode> path;
+            AStarPath(closeQue, beg, min_node, path);
+            return path;
+        }
+
+        closeQue.push_back(min_node);
+        OpenQueueErase(openQue, min_node);
+
+        int x_pos = XPosition(min_node);
+
+        /* min_node中x与上下左右4个方向的数字交换位置 */
+        for (int i = 0; i < 4; i++) {
+            int swap_position = x_pos + direction[i];
+            if (InsideBoundary(swap_position)) {
+                PuNode p;
+                deque<PuNode>::iterator result;
+                memcpy(p.value, min_node.value, 9);
+                SwapPosition(p, x_pos, swap_position);
+                p.gScore = min_node.gScore + 1;
+                p.hScore = HScoreDiff(p, end);
+                memcpy(p.fatherValue, min_node.value, 9);
+
+                /* 若close队列中已经存在p点 跳过 */
+                if (QueueContain(closeQue, p, result))
+                    continue;
+                /* 若close队列中不存在p点 open队列中存在p点 更新路径 */
+                if (QueueContain(openQue, p, result)) {
+                    /* 更新父节点和g值 获得更短路径 */
+                    if (p.gScore < result->gScore) {
+                        memcpy(result->fatherValue, min_node.value, 9);
+                        result->gScore = p.gScore;
+                    }
+                }
+                else {
+                    /* 若close队列中不存在p点 open队列中不存在p点 加入open队列 */
+                    openQue.push_back(p);
+                }
+            }
+        }
+    }
+
+    return vector<PuNode>();
 }
 
 #endif
