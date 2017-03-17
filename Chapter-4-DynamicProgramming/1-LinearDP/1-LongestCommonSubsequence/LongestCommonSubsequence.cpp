9b054f89d8679816485e0b95863eb02ab6fd6797
zhaochenyou <zhaochenyou16@gmail.com>
2017-03-17 14:01:28 +0800

    finish LongestCommonSubsequence

--- Chapter-4-DynamicProgramming/1-LinearDP/1-LongestCommonSubsequence/LongestCommonSubsequence.cpp
+++ Chapter-4-DynamicProgramming/1-LinearDP/1-LongestCommonSubsequence/LongestCommonSubsequence.cpp
@@ -1,29 +1,45 @@
-#include "HashTable.hpp"
-#include <assert.h>
-
+#include "LongestCommonSubsequence.hpp"
+#include <cassert>
 
 #define TEST_MAX 1024
 
+struct Test {
+    int a[MAX];
+    int b[MAX];
+    int n;
+    int result;
+} test_cases[] = {
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        { 0, 1, 3, 5, 7, 9, 11, 13, 15, 17 }, 
+        10, 5
+    },
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        { 0, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9 }, 
+        10, 5
+    },
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        { 0, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 
+        10, 1
+    },
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        { 0, 1, 2, 8, 7, 4, 3, 4, 3, 2, 1 }, 
+        10, 4
+    },
+};
+
+
 int main()
 {
-    HashTable *t = HashTableNew();
-    assert(t);
-
-    int tab[TEST_MAX];
-    for (int i = 0; i < TEST_MAX; i++) {
-        tab[i] = i;
+    int count = sizeof(test_cases) / sizeof(Test);
+    for (int i = 0; i < count; i++) {
+        Test & t = test_cases[i];
+        int r = LongestCommonSubsequence(t.a, t.b, t.n);
+        assert(r == t.result);
     }
-    for (int i = 0; i < TEST_MAX; i++) {
-        assert(HashTableInsert(t, tab[i], tab[i]) == 0);
-    }
-    for (int i = 0; i < TEST_MAX; i++) {
-        int p = HashTableFind(t, tab[i]);
-        assert(p == tab[i]);
-    }
-    for (int i = 0; i < TEST_MAX; i++) {
-        assert(HashTableRemove(t, tab[i]) == 0);
-    }
-    HashTableFree(t);
 
     return 0;
 }
--- Chapter-4-DynamicProgramming/1-LinearDP/1-LongestCommonSubsequence/LongestCommonSubsequence.hpp
+++ Chapter-4-DynamicProgramming/1-LinearDP/1-LongestCommonSubsequence/LongestCommonSubsequence.hpp
@@ -1,132 +1,36 @@
-#ifndef HASH_TABLE_HPP
-#define HASH_TABLE_HPP 1
+#ifndef LONGEST_COMMON_SUBSEQUENCE
+#define LONGEST_COMMON_SUBSEQUENCE
 
 #ifndef MAX
 #define MAX 1024
 #endif
-#include <stdint.h>
-#include <string.h>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-struct HashTableNode {
-    int key;
-    int value;
-    HashTableNode *next;
-};
 
-struct HashTable {
-    HashTableNode* tab[MAX];
-};
+// 序列a和b的范围是[1,n]
+int f[MAX][MAX];
 
-
-HashTable *HashTableNew()
+int LongestCommonSubsequence(const int a[MAX], const int b[MAX], int n)
 {
-    HashTable *t = new HashTable();
-
-    if (t == NULL) {
-        return NULL;
-    }
-    memset(t->tab, 0, sizeof(t->tab));
-    return t;
-}
-
-void HashTableFree(HashTable *t)
-{
+    // 初始化
     for (int i = 0; i < MAX; i++) {
-        HashTableNode *p = t->tab[i];
-        while (p) {
-            HashTableNode *q = p->next;
-            delete p;
-            p = q;
+        f[i][0] = 0;
+        f[0][i] = 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (a[i] == b[j])
+                f[i][j] = f[i-1][j-1] + 1;
+            else
+                f[i][j] = max( f[i-1][j], f[i][j-1] );
         }
     }
-    delete t;
+
+    return f[n][n];
 }
 
-uint32_t UIntMix(uint32_t key)
-{
-    key += ~(key << 15);
-    key ^=  (key >> 10);
-    key +=  (key << 3);
-    key ^=  (key >> 6);
-    key += ~(key << 11);
-    key ^=  (key >> 16);
-    return key;
-}
-
-int HashTableInsert(HashTable *t, int key, int value)
-{
-    HashTableNode *newnode = new HashTableNode();
-    if (newnode == NULL) {
-        return -1;
-    }
-
-    newnode->key = key;
-    newnode->value = value;
-    newnode->next = NULL;
-
-    unsigned int index = UIntMix(key) % MAX;
-    HashTableNode *p = t->tab[index];
-
-    if (p) {
-        HashTableNode *q = p->next;
-
-        while (q) {
-            p = q;
-            q = q->next;
-        }
-
-        p->next = newnode;
-    } else {
-        t->tab[index] = newnode;
-    }
-
-    return 0;
-}
-
-int HashTableFind(HashTable *t, int key)
-{
-    unsigned int index = UIntMix(key) % MAX;
-    HashTableNode *p = t->tab[index];
-
-    while (p) {
-        if (p->key == key) {
-            return p->value;
-        }
-        p = p->next;
-    }
-
-    return -1;
-}
-
-int HashTableRemove(HashTable *t, int key)
-{
-    unsigned int index = UIntMix(key) % MAX;
-    HashTableNode *p = t->tab[index];
-
-    if (!p) {
-        return -1;
-    }
-
-    if (p->key == key) {
-        t->tab[index] = p->next;
-        delete p;
-        return 0;
-    }
-
-    HashTableNode *q = p->next;
-    while (q && q->key != key) {
-        p = p->next;
-        q = p->next;
-    }
-    if (q && q->key == key) {
-        p->next = q->next;
-        delete q;
-        return 0;
-    }
-
-    return -1;
-}
 
 #endif
