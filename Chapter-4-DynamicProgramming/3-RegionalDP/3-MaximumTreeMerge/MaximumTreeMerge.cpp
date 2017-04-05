1bc3c5ea948193d667054d68b55266e80011437e
zhaochenyou <zhaochenyou16@gmail.com>
2017-04-05 18:46:40 +0800

    TODO: fix bug

--- Chapter-4-DynamicProgramming/3-RegionalDP/3-MaximumTreeMerge/MaximumTreeMerge.cpp
+++ Chapter-4-DynamicProgramming/3-RegionalDP/3-MaximumTreeMerge/MaximumTreeMerge.cpp
@@ -1,19 +1,55 @@
-#include "1_minimum_merge_cost.h"
+#include "MaximumTreeMerge.hpp"
+#include <cassert>
 #include <iostream>
-#include <string>
 using namespace std;
 
-void print(int *s, int len, string info)
-{
-	cout << info;
-	for(int i = 1; i <= len; ++ i)
-		cout << s[i] << ' ';
-	cout << endl;
-}
+#define TEST_MAX 1024
+
+struct TMTest {
+    int s[MAX];
+    int n;
+    int result;
+} test_cases[] = {
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7 },
+        7, 544
+    },      // 0
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        5, 30
+    },      // 1
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        5, 30
+    },      // 2
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        5, 30
+    },      // 3
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        5, 30
+    },      // 4
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        5, 30
+    },      // 5
+    { 
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+        5, 30
+    },      // 6
+};
+
+
 int main()
 {
-	int s1[7] = {0, 1, 1, 5, 4, 3, 6};
-	print(s1, 6, "s1: ");
-	cout << "minimum merge cost: " << minimum_merge_cost(s1, 6) << endl;
-	return(0);
+    int count = sizeof(test_cases) / sizeof(TMTest);
+    for (int i = 0; i < count; i++) {
+        TMTest & t = test_cases[i];
+        int r = MaximumTreeMerge(t.s, t.n);
+        cout << i << ": " << r << endl;
+        assert(r == t.result);
+    }
+
+    return 0;
 }
