efd3a263789d07594d13468a4e92f118ecdd72df
linronggbin <zhaochenyou16@gmail.com>
2017-07-08 23:05:18 +0800

    optimize code

--- src/CombinatorialMathematics/Combination.hpp
+++ src/CombinatorialMathematics/Combination.hpp
@@ -1,7 +1,6 @@
 #ifndef COMBINATION_HPP
 #define COMBINATION_HPP
 
-// #include <iostream>
 #include <cassert>
 #include <algorithm>
 #include <vector>
@@ -11,17 +10,10 @@ using namespace std;
 #define MAX 1024
 #endif
 
+
 void MakeChoice(vector<int> & choose, int prev, int prev_one, int s[MAX], int n, int m, vector<unordered_set<int>> & comb)
 {
     if (prev == m) {
-        /*
-        cout << "choose: ";
-        for (int i = 0; i < choose.size(); i++) {
-            cout << choose[i]<< " ";
-        }
-        cout << endl;
-        */
-
         unordered_set<int> tmp;
         for (int i = 0; i < choose.size(); i++) {
             if (choose[i] == 1)
@@ -45,23 +37,11 @@ void MakeChoice(vector<int> & choose, int prev, int prev_one, int s[MAX], int n,
 vector<unordered_set<int>> Combination(int s[MAX], int n, int m)
 {
     // 初始化choose=[0, 0, 0, ..., 0]
-    vector<int> choose;
-    for (int i = 0; i < n; i++) {
-        choose.push_back(0);
-    }
+    vector<int> choose(n, 0);
     vector<unordered_set<int>> comb;
-
-    // 初始时 1的数量为0个 最后一个1的位置为-1
+    // 初始时1的数量为0个 最后一个1的位置为-1
     MakeChoice(choose, 0, -1, s, n, m, comb);
 
-    /*
-    for (int i = 0; i < comb.size(); i++) {
-        for (unordered_set<int>::iterator j = comb[i].begin(); j != comb[i].end(); j++) {
-            cout << *j << " ";
-        }
-        cout << endl;
-    }
-    */
     return comb;
 }
 
