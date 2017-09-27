8d55d72a1ed59c01048c1a7feaa651559c105f4f
linronggbin <zhaochenyou16@gmail.com>
2017-09-27 08:28:14 +0800

    update source code with cpplint

--- src/Sort/BubbleSort.cpp
+++ src/Sort/BubbleSort.cpp
@@ -1,53 +1,47 @@
+// Copyright 2017 zhaochenyou16@gmail.com
+
 #include "BubbleSort.hpp"
 #include <iostream>
 #include <cassert>
 #include <cstdlib>
-using namespace std;
-
-#define MAX 64
-
-auto AssertNotAscendingOrder(vector<int> &s, int n) -> void
-{
-    bool res = true;
-    for (int i = 0; i < n-1; ++i) {
-        if (s[i] > s[i+1]) {
-            res = false;
-            break;
-        }
-    }
-    assert(!res);
-}
-auto AssertAscendingOrder(vector<int> &s, int n) -> void
-{
-    bool res = true;
-    for (int i = 0; i < n-1; ++i) {
-        if (s[i] > s[i+1]) {
-            res = false;
-            break;
-        }
-    }
-    assert(res);
-}
-auto AssertBubbleSort(vector<int> &s, int n) -> void
-{
-    AssertNotAscendingOrder(s, n);
-    BubbleSort(s, 0, n);
-    AssertAscendingOrder(s, n);
-}
-
 
 #define TEST_MAX 1024
+#define MAX 64
 
-auto main(void) -> int
-{
-    vector<int> s(MAX, 0);
-
-    for (int i = 0; i < TEST_MAX; ++i) {
-        for (int j = 0; j < MAX; ++j) {
-            s[j] = rand() % TEST_MAX;
-        }
-        AssertBubbleSort(s, MAX);
+auto AssertNotAscendingOrder(const vector<int> &s, int n) -> void {
+  bool res = true;
+  for (int i = 0; i < n-1; ++i) {
+    if (s[i] > s[i+1]) {
+      res = false;
+      break;
     }
+  }
+  assert(!res);
+}
+auto AssertAscendingOrder(const vector<int> &s, int n) -> void {
+  bool res = true;
+  for (int i = 0; i < n-1; ++i) {
+    if (s[i] > s[i+1]) {
+      res = false;
+      break;
+    }
+  }
+  assert(res);
+}
 
-    return 0;
+auto AssertBubbleSort(const vector<int> &s, int n) -> void {
+  AssertNotAscendingOrder(s, n);
+  BubbleSort(s, 0, n);
+  AssertAscendingOrder(s, n);
+}
+
+auto main(void) -> int {
+  vector<int> s(MAX, 0);
+  for (int i = 0; i < TEST_MAX; ++i) {
+    for (int j = 0; j < MAX; ++j) {
+      s[j] = rand() % TEST_MAX;
+    }
+    AssertBubbleSort(s, MAX);
+  }
+  return 0;
 }
--- src/Sort/BubbleSort.hpp
+++ src/Sort/BubbleSort.hpp
@@ -1,8 +1,11 @@
-﻿#ifndef SORT_BUBBLE_SORT_H
-#define SORT_BUBBLE_SORT_H
+﻿// Copyright 2017 zhaochenyou16@gmail.com
+
+#ifndef SRC_SORT_BUBBLESORT_HPP_
+#define SRC_SORT_BUBBLESORT_HPP_
 
 #include <vector>
 #include <algorithm>
+#include <utility>
 using namespace std;
 
 
@@ -13,13 +16,12 @@ using namespace std;
  * @param end       序列s是左闭右开区间[beg, end)
  */
 
-auto BubbleSort(vector<int> &s, int beg, int end) -> void
-{
-    for (int i = end-1; i >= beg; --i)
-        for (int j = beg; j < i; ++j)
-            if (s[j] > s[j+1])
-                swap(s[j], s[j+1]);
+auto BubbleSort(vector<int> &s, int beg, int end) -> void {
+  for (int i = end-1; i >= beg; --i)
+    for (int j = beg; j < i; ++j)
+      if (s[j] > s[j+1])
+        swap(s[j], s[j+1]);
 }
 
 
-#endif
+#endif  // SRC_SORT_BUBBLESORT_HPP_
