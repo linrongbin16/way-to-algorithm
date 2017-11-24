1367205d110ee59ba2580eaf037af6fccb3a1fea
zhaochenyou <zhaochenyou16@gmail.com>
2017-11-24 11:39:44 +0800

    fix code

--- src/GraphTheory/Traverse/BreadthFirstSearch.hpp
+++ src/GraphTheory/Traverse/BreadthFirstSearch.hpp
@@ -14,25 +14,26 @@ using namespace std;
 
 
 /* 总是从节点0开始进行遍历 */
-auto BreadthFirstSearch(int g[MAX][MAX], int n) -> vector<int> {
-  vector<int> sequence;
-  queue<int> que;
+std::vector<int> BreadthFirstSearch(int g[MAX][MAX], int n) {
+  std::vector<int> sequence;
+  std::queue<int> que;
   int visited[MAX];
   memset(visited, 0, sizeof(visited));
   que.push(0);
   visited[0] = 1;
 
-  while(not que.empty()) {
+  while(!que.empty()) {
     int e = que.front();
     que.pop();
     sequence.push_back(e);
 
     // for each node adjacent to e
-    for(int i = 0; i < n; i++)
-      if(i != e and g[e][i] and not visited[i]) {
+    for (int i = 0; i < n; i++) {
+      if (i != e && g[e][i] && !visited[i]) {
         que.push(i);
         visited[i] = 1;
       }
+    }
   }
   return sequence;
 }
