f32c710fa44dd1a4c2083014482fc19a790041d4
linrongbin <linrongbin16@gmail.com>
2018-09-04 15:46:58 +0800

    remove fontsettings

--- src/AnalyticGeometry/Polygon/CrossTest.cpp
+++ src/AnalyticGeometry/Polygon/CrossTest.cpp
@@ -2,10 +2,17 @@
 #include <cassert>
 using namespace std;
 
+struct Test {
+  Vec v1;
+  Vec v2;
+  double result;
+} tests[] = {{{1.0f, 1.0f}, {0.0f, 2.0f}, 0.1f}};
+
 int main() {
-  Vec v1(1.0f, 1.0f), v2(0.0f, 2.0f);
-  double c = Cross(v1, v2);
-  assert(c == (double)0.1f);
+  for (int i = 0; i < sizeof(tests) / sizeof(Test); i++) {
+    Test &t = tests[i];
+    assert(Cross(t.v1, t.v2) == t.result);
+  }
   return 0;
 }
 
