8cc60497370273fd6ab50f36e2a813e4a52b5fe9
damu <damu@pinduoduo.com>
2019-01-16 15:05:31 +0800

    update docs, add source code

--- src/DataStructure/SkipList.cpp
+++ src/DataStructure/SkipList.cpp
@@ -1,9 +1,57 @@
 #include "SkipList.h"
+#include <cassert>
+#include <cmath>
+#include <string>
 
-SlNode *SkipListNew() {}
+#define MIN_LEVEL 4
+SkNode SKNIL = {-1, NULL};
 
-void SkipListFree(SlNode *l) {}
-void SkipListInsert(SlNode *l, int value) {}
-SlNode *SkipListFind(SlNode *l, int value) {}
-void SkipListErase(SlNode *l, int value) {}
+static void SkNodeInit(SkNode *e, int value, int maxlv) {
+  e->value = value;
+  e->link = new SkNode *[maxlv];
+  for (int i = 0; i < maxlv; i++) {
+    set_nil(e->link[i]);
+  }
+}
+
+SkList *SkipListNew(int node_count) {
+  assert(node_count > 0);
+  int maxlv = std::log2(node_count);
+
+  SkList *l = new SkList();
+  l->size = 0;
+  l->max_level = (maxlv > MIN_LEVEL) ? maxlv : MIN_LEVEL;
+  SkNodeInit(&l->head, -1, maxlv);
+  return l;
+}
+
+void SkipListFree(SkList *l) {
+  assert(l);
+  SkNode *e = l->head.link[0], *p;
+  while (not_nil(e)) {
+    p = e;
+    e = e->link[0];
+    delete p->link;
+    delete p;
+  }
+  delete l;
+}
+
+int SkipListSize(SkList *l) { return l->size; }
+
+void SkipListInsert(SkList *l, int value) {
+  SkNode *e = new SkNode();
+  SkNodeInit(e, value, l->max_level);
+
+  SkNode *p = l->head.link[l->max_level - 1];
+  for (int i = l->max_level - 1; i >= 0; i--) {
+    while (not_nil(p) && p->value > value) {
+      p = p->link[i];
+    }
+  }
+}
+
+SkNode *SkipListFind(SkList *l, int value);
+
+void SkipListErase(SkList *l, int value);
 
--- src/DataStructure/SkipList.h
+++ src/DataStructure/SkipList.h
@@ -1,15 +1,41 @@
 #pragma once
-#include <algorithm>
-#include <cassert>
-#include <utility>
+#include <vector>
 
 struct SkNode {
   int value;
+
+  // link[0] is lowest link, link[max_level-1] is highest link
+  // pointer to next node
+  SkNode **link;
 };
 
-SkNode *SkipListNew();
-void SkipListFree(SlNode *l);
-void SkipListInsert(SlNode *l, int value);
-SlNode *SkipListFind(SlNode *l, int value);
-void SkipListErase(SlNode *l, int value);
+struct SkList {
+  int size;
+  int max_level;
+  SkNode head;
+};
+
+extern SkNode SKNIL;
+
+#define is_nil(e) ((e) == &SKNIL)
+#define not_nil(e) (!is_nil(e))
+#define set_nil(e) ((e) = &SKNIL)
+
+// create skip list
+SkList *SkipListNew(int node_count);
+
+// delete skip list
+void SkipListFree(SkList *l);
+
+// skip list size
+int SkipListSize(SkList *l);
+
+// insert value to skip list
+void SkipListInsert(SkList *l, int value);
+
+// find value in skip list
+SkNode *SkipListFind(SkList *l, int value);
+
+// erase value from skip list
+void SkipListErase(SkList *l, int value);
 
